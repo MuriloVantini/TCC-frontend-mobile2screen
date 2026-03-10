ad0d1acf2d235d137dd1ee36fa0f2038e202ee8b
MuriloVantini <m.henrique.vantini18@gmail.com>
2026-03-10 08:58:15 -0300

    feat: adicionar sonner no escopo global do projeto

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { UserContextProvider } from "./contexts/UserContextProvider";
+import { Toaster } from "./components/ui/sonner";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <div className="min-h-screen bg-background text-foreground transition-colors duration-300">
         <RouterProvider router={router} />
       </div>
+      <Toaster />
     </UserContextProvider>
   );
 }
